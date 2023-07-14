15f2953df55e89a30eb7bcda7d22ff7a5f095400
An.Vu <vuquocan1987@gmail.com>
2023-07-15 06:28:34 +0930

    Commit the init file

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
-sh Anaconda3-2021.11-Linux-x86_64.sh
+sh Anaconda3-2021.11-Linux-x86_64.sh -b
 git clone https://github.com/vuquocan1987/StereotypeWords.git
 cd StereotypeWords
 conda env create -f textdebias.yaml
