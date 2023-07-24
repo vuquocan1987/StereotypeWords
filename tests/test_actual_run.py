3593ca3c48901e36b54bfe810f6a2af08bbee3c2
An.Vu <vuquocan1987@gmail.com>
2023-07-24 21:55:18 +0930

    change test files

--- tests/test_actual_run.py
+++ tests/test_actual_run.py
@@ -4,57 +4,12 @@ import config as cf
 import my_main
 
 
-@pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
-@pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.Idiom])
-def test_actual_run_idiom(dataset_name,base_model,stereotype):
-    cf.Round=1
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-
-    my_main.MAIN()
-@pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
-@pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.Normal])
-def test_actual_run_init(dataset_name,base_model,stereotype):
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-
-    my_main.MAIN()
-
-@pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
-@pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.RandomMask, cf.StereoType.Noun])
-def test_actual_run_RCNN_CNN(dataset_name,base_model,stereotype):
-    cf.Round=1
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-
-    my_main.MAIN()
-
-@pytest.mark.parametrize("dataset_name", ["Yelp_Hotel", "News","Economy","Amazon","Parties", "ChemProt","SCIERC", "ARC" , "HyperPartisan", "Twitter",])
-@pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.Noun])
-def test_actual_run_Noun_RCNN_CNN(dataset_name,base_model,stereotype):
-    cf.Round=1
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-
-    my_main.MAIN()
-
-@pytest.mark.parametrize("dataset_name", ["Yelp_Hotel", "News","Economy","Amazon","Parties", "ChemProt","SCIERC", "ARC" , "HyperPartisan", "Twitter",])
+@pytest.mark.parametrize("dataset_name", ["Economy","Amazon","Parties","SCIERC", "ARC" , "HyperPartisan", "Twitter"])
 @pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
 @pytest.mark.parametrize("stereotype", [cf.StereoType.RandomMask])
-def test_actual_run_RandomMask_RCNN_CNN(dataset_name,base_model,stereotype):
+def test_actual_run_everything_light(dataset_name,base_model,stereotype):
     cf.Round=1
+    cf.Batch
     cf.DATA_PATH = './data/'
     cf.Dataset_Name = dataset_name
     cf.Base_Model = base_model
@@ -67,7 +22,7 @@ def test_actual_run_RandomMask_RCNN_CNN(dataset_name,base_model,stereotype):
 @pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
 @pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
 @pytest.mark.parametrize("stereotype", [cf.StereoType.Keyword])
-def test_smoke_bigram(dataset_name,base_model,stereotype):
+def test_actual_bigram(dataset_name,base_model,stereotype):
     cf.Round = 1
     cf.DATA_PATH = './data/'
     cf.Dataset_Name = dataset_name
@@ -77,31 +32,6 @@ def test_smoke_bigram(dataset_name,base_model,stereotype):
     cf.Alpha = 1
     my_main.MAIN()
 
-
-@pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
-@pytest.mark.parametrize("base_model", ["RoBERTa"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.Normal, cf.StereoType.Keyword, cf.StereoType.Imbword, cf.StereoType.RandomMask, cf.StereoType.Noun])
-def test_smoke_roberta(dataset_name,base_model,stereotype):
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-    my_main.MAIN()
-
-
-@pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "News","Parties", "SCIERC","Twitter","Yelp_Hotel"])
-@pytest.mark.parametrize("base_model", ["RoBERTa"])
-@pytest.mark.parametrize("stereotype", [cf.StereoType.Normal, cf.StereoType.Keyword, cf.StereoType.Imbword])
-def test_smoke_bigram_roberta(dataset_name,base_model,stereotype):
-    cf.DATA_PATH = './data/'
-    cf.Dataset_Name = dataset_name
-    cf.Base_Model = base_model
-    cf.Stereotype = stereotype
-    cf.N_GRAM = 2
-    cf.Alpha = 1
-    my_main.MAIN()
-<<<<<<< HEAD
-=======
 
 @pytest.mark.parametrize("dataset_name", ["Amazon", "ARC" , "ChemProt", "Economy", "HyperPartisan", "Parties", "SCIERC","Twitter",]) # "Yelp_Hotel","News",
 @pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
@@ -113,4 +43,19 @@ def test_actual_Idiom(dataset_name,base_model,stereotype):
     cf.Base_Model = base_model
     cf.Stereotype = stereotype
     my_main.MAIN()
->>>>>>> a7c692b23a3321c9aa988653f51b046ce105ed06
+
+
+
+@pytest.mark.parametrize("dataset_name", ["Yelp_Hotel", "News","Economy","Amazon","Parties", "ChemProt","SCIERC", "ARC" , "HyperPartisan", "Twitter",])
+@pytest.mark.parametrize("base_model", ["TextRCNN","TextCNN"])
+@pytest.mark.parametrize("stereotype", [cf.StereoType.Normal])
+def test_get_init(dataset_name,base_model,stereotype):
+    cf.Round = 1
+    cf.Init_epoch = 1
+    cf.DATA_PATH = './data/'
+    cf.Dataset_Name = dataset_name
+    cf.Base_Model = base_model
+    cf.Stereotype = stereotype
+    my_main.MAIN()
+
+
